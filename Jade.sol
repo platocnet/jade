6d177da2be35f7ad2f0af7f1e9c4767634f31fef
zjq <platocnet@163.com>
2018-06-29 15:02:14 +0800

    ready for beta

--- Jade.sol
+++ Jade.sol
@@ -90,7 +90,7 @@ contract Jade {
 
     // all call_func from msg.sender must at least have 50 ptc coins
     modifier only_ptc_owner {
-        // require(ptc_ins.balanceOf(msg.sender) >= tickets;
+        require(ptc_ins.balanceOf(msg.sender) >= tickets);
         _;
     }
 
